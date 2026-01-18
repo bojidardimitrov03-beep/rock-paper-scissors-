93e93aa444bba61ac026dd4dd9e494fa585000fb
Bojidar <254455568+bojidardimitrov03-beep@users.noreply.github.com>
2026-01-18 13:33:20 +0200

    playround ready

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
-function getComputerChoice(){
-    const random = Math.random();
-    if (random<0.33) {
-        return "rock";
-    } else if (random<0.66) {
-        return "paper";
-    } else {
-        return "scissors"
-    }
+let humanScore = 0;
+let computerScore = 0;
+
+
+function getComputerChoice() {
+  const random = Math.random();
+  
+  if (random < 0.33) {
+    return 'rock';
+  } else if (random < 0.66) {
+    return 'paper';
+  } else {
+    return 'scissors';
+  }
 }
+
+// Test it
 console.log(getComputerChoice());
 
-function chooseWeapon(weapon){
-    const result = document.getElementById('result');
-    result.textContent = "You chose:" + weapon;
+function getHumanChoice(){
+    const choice = prompt("enter rock, paper, or scissors:");
+    return choice;
+
 }
-let HumanScore = 0
-let computerScore = 0 
+console.log(getHumanChoice());
 
-function playRound(chooseWeapon, getComputerChoice){
-    
+function playRound(humanChoice, ComputerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    console.log (`you chose: ${humanChoice}`);
+    console.log (`Computer chose: ${computerChoice}`)
 
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!")
+        return;
+ 
+    }
+    if (
+        (humanChoice === 'rock'&& computerChoice ==="scissors")||
+        (humanChoice === 'paper'&& computerChoice ==="rock")||
+        (humanChoice === 'scissors'&& computerChoice ==="paper")
+    ){
+        console.log(`you Win! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+
+    } else{
+        console.log(`you lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
 }
