752470d97970c59258a66810e9b0171fb37fa185
Bojidar <254455568+bojidardimitrov03-beep@users.noreply.github.com>
2026-01-18 13:03:48 +0200

    computer choice is done

---
+++ script.js
@@ -0,0 +1,11 @@
+function getComputerChoice(){
+    const random = Math.random();
+    if (random<0.33) {
+        return "rock";
+    } else if (random<0.66) {
+        return "paper";
+    } else {
+        return "scissors"
+    }
+}
+console.log(getComputerChoice());
